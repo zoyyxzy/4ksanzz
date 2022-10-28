1694b8c33627f906a298b32fcfcea43f5d9dabf7
Fadil Xd <92127052+DILSTORY@users.noreply.github.com>
2022-10-28 09:44:37 +0800

    update config.js

--- config.js
+++ config.js
@@ -43,7 +43,7 @@ global.mess = {
 }
 global.limitawal = {
     premium: "Infinity",
-    free: 25
+    free: 100
 }
 global.thumb = fs.readFileSync('./image/naze.jpg')
 global.vaze = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
